aecf0a4c26b50df843f5aca7e3f29c49064163db
Maciej Nowakowski <m.nowakowskipl@gmail.com>
2016-04-10 12:55:13 +0100

    ValidatedMethod works only on client

--- imports/api/myprofile/collections.js
+++ imports/api/myprofile/collections.js
@@ -33,16 +33,16 @@ UserProfiles.publicFields = {
   pictureUrl: 1
 }
 
-UserProfiles.allow({
-  insert() { return false; },
-  update() { return false; },
-  remove() { return false; }
-});
-
-UserProfiles.deny({
-  insert() { return true; },
-  update() { return true; },
-  remove() { return true; }
-});
+// UserProfiles.allow({
+//   insert() { return false; },
+//   update() { return false; },
+//   remove() { return false; }
+// });
+//
+// UserProfiles.deny({
+//   insert() { return true; },
+//   update() { return true; },
+//   remove() { return true; }
+// });
 
 export default UserProfiles;
--- imports/api/myprofile/methods.js
+++ imports/api/myprofile/methods.js
@@ -6,7 +6,19 @@ export const createProfile = new ValidatedMethod({
   name: 'UserProfiles.createProfile',
   validate: UserProfiles.Schema.validator(),
   run({ userId, name, description = '', pictureUrl = '' }) {
-      const profile = { userId, name, description, pictureUrl };
-      UserProfiles.insert( profile );
+      const profile = {
+        userId,
+        name,
+        description,
+        pictureUrl
+      };
+      console.log(profile);
+      
+      if (Meteor.isServer) {
+        return UserProfiles.insert( profile );
+        console.log("server!");
+      } else {
+        console.log('client!');
+      }
   }
 });
